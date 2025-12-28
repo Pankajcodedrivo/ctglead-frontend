edd668c31d5d3067e9c36592ad3959f42a522b85
poulami-kuti <108513318+Poulami-1@users.noreply.github.com>
2025-12-28 16:06:04 +0530

    updated

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 import Login from "../pages/auth/Login/Login";
 import VerifyEmail from "../pages/auth/Login/VerifyEmail"
 import VerifySecurityCode from "../pages/auth/Login/VerifySecurityCode"
-import MyProfile from "../pages/MyProfile";
+import Dashboard from "../pages/Dashboard";
 import MyLeads from "../pages/MyLeads";
 import MyQuotes from "../pages/MyQuotes";
 import SellPolicy from "../pages/SellPolicy";
@@ -38,8 +38,8 @@ export default function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="/verify-security-code" element={<VerifySecurityCode />} />
-        {/* my profile */}
-        <Route path="/my-profile" element={<MyProfile />} />
+        {/* Dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/my-leads" element={<MyLeads />} />
         <Route path="/my-quotes" element={<MyQuotes />} />
         <Route path="/sell-policy" element={<SellPolicy />} />
